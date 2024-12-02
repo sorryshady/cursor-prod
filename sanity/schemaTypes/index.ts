0e4b35ef0ce839959f45ec95bcbf8713c8da46e3
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-12-02 14:04:14 +0530

    removed sanity folder

--- sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import downloads from './downloads'
-import gallery from './gallery'
-import news from './news'
-import upcomingEvent from './upcoming-events'
-import newsletter from './newsletter'
-export const schemaTypes = [news, upcomingEvent, downloads, gallery, newsletter]
