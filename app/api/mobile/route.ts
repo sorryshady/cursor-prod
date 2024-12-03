a832788be6904d15f85eeae2de6cd398d2cf20c6
Akhil Nishad <akhilnishadofficial@gmail.com>
2024-12-04 00:42:46 +0530

    photo upload via phone also working

--- app/api/mobile/route.ts
+++ app/api/mobile/route.ts
@@ -1,6 +1,7 @@
-import { auth } from '@/lib/auth/auth'
+import { auth } from "@/lib/auth/auth";
+import { prisma } from "@/lib/db";
+import { utapi } from "@/lib/utapi";
 import { NextRequest, NextResponse } from "next/server";
-
 
 export async function GET(req: NextRequest) {
   const user = await auth();
@@ -8,4 +9,53 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
   return NextResponse.json(user);
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    // Verify user authentication
+    const session = await auth();
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // Get form data from request
+    const formData = await req.formData();
+    const photo = formData.get("photo") as File;
+
+    if (!photo) {
+      return NextResponse.json({ error: "No photo provided" }, { status: 400 });
+    }
+    const uploadedFile = await utapi.uploadFiles(photo);
+    if (!uploadedFile.data?.url || !uploadedFile.data?.key) {
+      return NextResponse.json(
+        { error: "Failed to upload photo" },
+        { status: 500 },
+      );
+    }
+    if (session.photoId) {
+      await utapi.deleteFiles(session.photoId);
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { membershipId: session.membershipId! },
+      data: {
+        photoId: uploadedFile.data?.key,
+        photoUrl: uploadedFile.data?.url,
+      },
+      select: {
+        photoUrl: true,
+      },
+    });
+
+    return NextResponse.json({
+      photoUrl: updatedUser.photoUrl,
+    });
+  } catch (error) {
+    console.error("Error in update-photo route:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
 }
